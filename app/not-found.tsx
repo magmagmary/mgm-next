0efb0b16830358b546bb06e29f9942f50e58c22e
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-22 12:06:57 +0100

    chore: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+
+const NotFoundPage = () => {
+  return (
+    <main className="flex items-center justify-center h-full">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+    </main>
+  );
+};
+
+export default NotFoundPage;
