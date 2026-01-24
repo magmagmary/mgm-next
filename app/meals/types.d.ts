c8c586d4bbcaccee3b2194073936d37e6b783db0
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-24 09:29:06 +0100

    feat: add meals db

--- app/meals/types.d.ts
+++ app/meals/types.d.ts
@@ -1,7 +1,10 @@
 export type Meal = {
+   id: number;
    title: string;
    slug: string;
    image: string;
    summary: string;
+   instructions: string;
    creator: string;
+   creator_email: string;
 }
