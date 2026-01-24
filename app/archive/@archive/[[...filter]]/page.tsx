fc8d1c8fe2820a59eb9d929dfe70b21b9fd4a1cb
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-24 22:28:01 +0100

    feat: catch-all routes

---
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -0,0 +1,46 @@
+import NewsList from '@/app/components/shared/news-list';
+import { News } from '@/app/news/types';
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from '@/lib/utils/news';
+import Link from 'next/link';
+
+const SpecificYearArchivePage = async ({ params }: { params: Promise<{ filter?: string[] }> }) => {
+  const { filter } = await params;
+  const [year, month] = filter || [];
+  const years = getAvailableNewsYears();
+
+  let news: News[] = [];
+  const months: number[] =getAvailableNewsMonths(+year!);
+  
+  if(year && !month) {
+    news = getNewsForYear(+year);
+  }
+
+  if(year && month) {
+    news = getNewsForYearAndMonth(+year, +month);
+  }
+
+  return <div className="flex flex-col gap-4">
+  <div className="flex gap-4 mt-5">
+    <span className='text-md  text-gray-300'>Years:</span>
+    {years.map((year) => (
+      <Link key={year} href={`/archive/${year}`} className="text-lg font-bold text-gray-300">{year}</Link>
+    ))}
+  </div>
+  {months.length > 0 && (
+    <div className="flex gap-4">
+      <span className='text-md text-gray-300'>Months:</span>
+      {months.map((month) => (
+        <Link key={month} href={`/archive/${year}/${month}`} className="text-lg font-bold text-gray-300">{month}</Link>
+      ))}
+    </div>
+  )}
+
+  {news.length > 0 ?  <NewsList news={news} /> 
+  : year  ? <p className='text-gray-500'>No news found for this year</p> 
+  : <p className='text-gray-500'>You need to select a year to view the archive</p>
+  }
+   
+  </div>
+}
+
+export default SpecificYearArchivePage
--- app/archive/@archive/page.tsx
+++
@@ -1,8 +0,0 @@
-const ArchivePage = () => {
-
-  return (
-    <></>
-  )
-}
-
-export default ArchivePage
