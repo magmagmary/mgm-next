9fdcaed95af85bc8dcfdd127ea05431168e25070
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-25 17:23:24 +0100

    feat: navigate back when clicking on the modal overlay

--- app/news/[slug]/@modal/(.)image/page.tsx
+++ app/news/[slug]/@modal/(.)image/page.tsx
@@ -1,9 +1,8 @@
 
 import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "@/lib/mockData/mockNews";
-import Image from "next/image";
 import { News } from "@/app/news/types";
-import { Dialog, DialogHeader, DialogContent ,DialogTitle ,DialogDescription} from "@/components/ui/dialog";
+import NewsModal from "@/app/news/components/news-modal";
 
 const NewsImagePage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
@@ -19,17 +18,7 @@ const NewsImagePage = async ({ params }: { params: Promise<{ slug: string }> }) 
   }
 
   return (
-    <Dialog open >
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{news.title}</DialogTitle>
-          <DialogDescription>
-          <Image src={`/images/${news.image}`} alt={news.title} width={500} height={500} className="object-cover my-5" />
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  
+   <NewsModal news={news} />
   )
 }
 
