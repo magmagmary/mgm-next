2619e549d343eedcc38a1cb6185c6a42c9f01811
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-25 17:26:12 +0100

    chore: clean-up home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
-import { Dialog, DialogHeader ,DialogTrigger , DialogContent ,DialogTitle ,DialogDescription} from "./components/ui/dialog";
 
 export default function Home() {
   return (
    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-4xl font-bold">Home page</h1>
-
-      <Dialog>
-      <DialogTrigger>Open Dialog</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+      <h1 className="text-4xl font-bold mb-5">Home page</h1>
    </div>
   );
 }
