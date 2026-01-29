dfd0b34cdd66875fa743836af6d6a7c6dedb4804
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-29 11:20:20 +0100

    feat: optimistic update

--- app/new-post/components/heart-icon.tsx
+++ app/new-post/components/heart-icon.tsx
@@ -2,21 +2,25 @@
 import { likePost } from '@/lib/actions/like-post';
 import { cn } from '@/lib/utils/utils';
 import { Heart } from 'lucide-react';
-import { useTransition } from 'react';
+import { useOptimistic, useTransition } from 'react';
 
 const likeClassName = 'fill-red-500 text-red-500';
 
 const HeartIcon = ({ postId , isLiked}: { postId: number, isLiked: boolean }) => {
-    const[isPending, startTransition] = useTransition();
+    const [optimisticLikesStatus, setOptimisticLikesStatus] = useOptimistic(isLiked, (prevIsLiked) => !prevIsLiked);
+
+    const[isPending, startTransition] = useTransition(); // with optimistic updates, we don't need to use useTransition but kept this for future reference
 
     const handleLike = async () => {
         startTransition(async () => {
+            setOptimisticLikesStatus(optimisticLikesStatus); // Pass any value, reducer will toggle it
             await likePost(postId);
         });
     }
 
   return (
-    <Heart className={cn(isLiked ? likeClassName : 'hover:fill-red-500 cursor-pointer hover:text-red-500' , isPending && 'animate-pulse animate-infinite')} onClick={handleLike} />
+    <Heart className={cn(optimisticLikesStatus ? likeClassName : 'hover:fill-red-500 cursor-pointer hover:text-red-500' , 
+        isPending && 'animate-pulse animate-infinite')} onClick={handleLike} />
   )
 }
 
