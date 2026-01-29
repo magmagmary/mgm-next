bd457a7eac1111190937af0f32266caabd03bc08
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-29 11:32:48 +0100

    chore: clean-up home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Posts from "@/components/shared/posts";
-import { Separator } from "@radix-ui/react-separator";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-full max-w-2xl mx-auto">
       <h1 className="text-4xl font-bold">Home</h1>
-      <Separator className="my-4 w-full h-[1px] bg-foreground" />
-      <Posts posts={[]} />
     </div>
   );
 }
