3b0db7361456dceab2010b4eb0584a83f1bf497f
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-01-31 11:54:52 +0100

    feat: training page

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -5,16 +5,20 @@ import Image from 'next/image';
 export default async function TrainingPage() {
   const trainingSessions = getTrainings();
 
+  console.log(trainingSessions);
+
   return (
-    <main>
-      <h1>Find your favorite activity</h1>
-      <ul id="training-sessions">
+    <main className="flex flex-col items-center justify-center p-4 max-w-5xl mx-auto">
+      <h1 className="text-2xl font-bold mb-5">Find your favorite activity</h1>
+      <ul id="training-sessions" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {trainingSessions.map((training: Training) => (
-          <li key={training.id}>
-            <Image src={`/trainings/${training.image}`} alt={training.title} />
-            <div>
-              <h2>{training.title}</h2>
-              <p>{training.description}</p>
+          <li key={training.id} className="flex flex-col items-center justify-center border border-gray-200 rounded-md overflow-hidden">
+            <div className="relative w-full h-40">
+            <Image src={`/trainings${training.image}`} fill alt={training.title} className="object-cover" />
+            </div>
+            <div className="p-4">
+              <h2 className="text-lg font-bold">{training.title}</h2>
+              <p className="text-sm text-gray-500">{training.description}</p>
             </div>
           </li>
         ))}
